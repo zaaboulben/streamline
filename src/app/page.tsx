144c00f7a0630df3f3cd0ea1f6c8b6369a861f4f
zaaboulben <benyamine.zaaboul@hotmail.com>
2023-11-27 02:50:40 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,8 @@ export default function Home() {
       if (response.authResponse) {
        console.log('Welcome!  Fetching your information.... ');
        window.FB.api('/me', function(response) {
-         console.log('Good to see you, ' + response.name + '.');
+        //@ts-ignore
+         console.log('Good to see you, ' + response .name + '.');
        });
       } else {
        console.log('User cancelled login or did not fully authorize.');
